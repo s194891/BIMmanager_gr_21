8604a2f996face52f91a069545d4661f208d3078
s194891 <s194891@dtu.dk>
2024-09-23 16:08:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 import ifcopenshell
-import sys
 import os
-
-# Få den nuværende sti til 'main.py'
-#current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Tilføj 'rules'-mappen til sys.path
-#sys.path.append(os.path.join(current_directory, 'external'))
-#sys.path.append(os.path.join(current_directory, 'GROUP_27'))
-#sys.path.append(os.path.join(current_directory, 'rules'))
-
-#from external.GROUP_27.rules.doorRule import doorRule
-import helloVstudio
-helloVstudio.checkRule()
-from external.BIManalyst_g_xy.rules import doorRule
-from external.GROUP_27.rules import columnRule
-
 
 # Find stien til modellen
 models_directory = r'C:\Users\julie\Google Drev\10.Semester\41934 Advanced BIM'
 # Filnavnet på modellen
-
 model_filename = 'CES_BLD_24_06_STR.ifc'
-#model_filename = 'LLYN - STRU.ifc'
-
 # Saml hele stien til IFC-filen
 model_path = os.path.join(models_directory, model_filename)
 model = ifcopenshell.open(model_path)
 
+from external.BIManalyst_g_xy.rules import doorRule
+from external.BIManalyst_g_22.rules import group22_function_get_columns_on_level
+#from external.BIManalyst_g_23.rules import TotalHeightWallsRule
+#from external.BIManalyst_g_26.rules import 
+#from external.BIManalyst_g_25.rules import numberElementCheck
+#from external.BIManalyst_g_27.rules import columnRule
+from external.BIManalyst_g_28.rules import storeysRule
+
 # Run all functions
 doorResult = doorRule.checkRule(model)
-#elNrResult = amountElement(model, IfcBeam, 2)
-columnResult = columnRule.checkRule(model)
+colOnLvResult = group22_function_get_columns_on_level.get_columns_on_level(model, 3)
+#totalHResult = TotalHeightWallsRule.checkRule(model)
+#amountElResult = numberElementCheck.amountElement(model, IfcBeam, 25)
+#columnRuleResult = columnRule.checkRule(model)
+storeysRuleResult = storeysRule.checkRule(model)
 
 # see the results
 print("Door result:", doorResult)
-print("column result:", columnResult)
+print("colOnLvResult:", colOnLvResult)
+#print("total height Result ", totalHResult)
+#print("amount el Result ", amountElResult)
+#print("column Result", columnRuleResult)
+print("storeys Result", storeysRuleResult)
