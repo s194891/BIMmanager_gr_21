626cda79c5f600cf4eaf0b9a4140c16fec8d5ef4
s194891 <s194891@dtu.dk>
2024-09-23 14:51:32 +0200

    update?

---
+++ helloVstudio.py
@@ -0,0 +1,5 @@
+def checkRule():
+    print("hello Vstudio")
+    test = 2+2
+
+pass
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import ifcopenshell
+import sys
+import os
 
-from .external.BIManalyst_g_xy.rules import windowRule
-from .external.BIManalyst_g_xy.rules import doorRule
+# Få den nuværende sti til 'main.py'
+#current_directory = os.path.dirname(os.path.abspath(__file__))
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+# Tilføj 'rules'-mappen til sys.path
+#sys.path.append(os.path.join(current_directory, 'external'))
+#sys.path.append(os.path.join(current_directory, 'GROUP_27'))
+#sys.path.append(os.path.join(current_directory, 'rules'))
 
-windowResult = windowRule.checkRule(model)
+#from external.GROUP_27.rules.doorRule import doorRule
+import helloVstudio
+helloVstudio.checkRule()
+from external.BIManalyst_g_xy.rules import doorRule
+from external.GROUP_27.rules import columnRule
+
+
+# Find stien til modellen
+models_directory = r'C:\Users\julie\Google Drev\10.Semester\41934 Advanced BIM'
+# Filnavnet på modellen
+
+model_filename = 'CES_BLD_24_06_STR.ifc'
+#model_filename = 'LLYN - STRU.ifc'
+
+# Saml hele stien til IFC-filen
+model_path = os.path.join(models_directory, model_filename)
+model = ifcopenshell.open(model_path)
+
+# Run all functions
 doorResult = doorRule.checkRule(model)
+#elNrResult = amountElement(model, IfcBeam, 2)
+columnResult = columnRule.checkRule(model)
 
-print("Window result:", windowResult)
+# see the results
 print("Door result:", doorResult)
+print("column result:", columnResult)
