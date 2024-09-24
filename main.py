e19b475bb2fe3eab9dd5b1c2e5bd0e73a46cd6e6
s194891 <s194891@dtu.dk>
2024-09-24 10:59:52 +0200

    main edit

--- main.py
+++ main.py
@@ -11,24 +11,29 @@ model = ifcopenshell.open(model_path)
 
 from external.BIManalyst_g_xy.rules import doorRule
 from external.BIManalyst_g_22.rules import group22_function_get_columns_on_level
-#from external.BIManalyst_g_23.rules import TotalHeightWallsRule
-#from external.BIManalyst_g_26.rules import 
-#from external.BIManalyst_g_25.rules import numberElementCheck
-#from external.BIManalyst_g_27.rules import columnRule
+from external.BIManalyst_g_23.rules import TotalHeightWallsRule
+from external.BIManalyst_g_24.rules import Calcualting_surfaceload_slabs
+from external.BIManalyst_g_25.rules import nameElement
+#from external.BIManalyst_g_26.rules import A1_python_code
+from external.BIManalyst_g_27.rules import column_AreaRule
 from external.BIManalyst_g_28.rules import storeysRule
 
 # Run all functions
 doorResult = doorRule.checkRule(model)
 colOnLvResult = group22_function_get_columns_on_level.get_columns_on_level(model, 3)
-#totalHResult = TotalHeightWallsRule.checkRule(model)
-#amountElResult = numberElementCheck.amountElement(model, IfcBeam, 25)
-#columnRuleResult = columnRule.checkRule(model)
+totalHResult = TotalHeightWallsRule.checkRule(model)
+#slabResult = Calcualting_surfaceload_slabs.checkRule(model)
+nameResult = nameElement.specifikElement(model)
+#beamResult = A1_python_code.checkRule(model)
+columnResult = column_AreaRule.checkRule(model)
 storeysRuleResult = storeysRule.checkRule(model)
 
 # see the results
 print("Door result:", doorResult)
 print("colOnLvResult:", colOnLvResult)
-#print("total height Result ", totalHResult)
-#print("amount el Result ", amountElResult)
-#print("column Result", columnRuleResult)
+print("total height Result ", totalHResult)
+print("slab result:",slabResult)
+print("name result:",nameResult)
+#print("beam Result", beamResult)
+print("column result", columnResult)
 print("storeys Result", storeysRuleResult)
